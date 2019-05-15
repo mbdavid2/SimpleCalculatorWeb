229d2d80aa74d62cd400ff08d0816898535a5ad5
mbdavid2 <mbdavid297@gmail.com>
2019-05-15 11:27:24 +0200

    Update CalculatorController.java

--- src/main/java/com/david/simplecalculator/CalculatorController.java
+++ src/main/java/com/david/simplecalculator/CalculatorController.java
@@ -12,8 +12,36 @@ public class CalculatorController {
         return "Salu2";
     }
 
-    @RequestMapping("/{left}/{operation}/{right}")
+    @RequestMapping("/{left}/{op}/{right}")
     public String OperationRequest(@PathVariable("left") Integer left, @PathVariable("operation") String operation, @PathVariable("right") Integer right) {
        return CalculatorService.performMathOperation(left, operation, right);
+    }
+
+    @RequestMapping("/add/{left}/{right}")
+    public String performAddOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
+    	Integer operationResult = CalculatorService.operationAdd(left, right);
+    	return Integer.toString(operationResult);
+    }
+
+    @RequestMapping("/sub/{left}/{right}")
+    public String performSubOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
+    	Integer operationResult = CalculatorService.operationSub(left, right);
+    	return Integer.toString(operationResult);
+    }
+
+    @RequestMapping("/mult/{left}/{right}")
+    public String performMultOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
+    	Integer operationResult = CalculatorService.operationMult(left, right);
+    	return Integer.toString(operationResult);
+    }
+
+    @RequestMapping("/div/{left}/{right}")
+    public String performDivOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
+    	if (right != 0) {
+    		Integer operationResult = CalculatorService.operationDiv(left, right);
+    		return Integer.toString(operationResult);
+    	}
+    	else {
+    		return "Error: division by 0";
     }
 }

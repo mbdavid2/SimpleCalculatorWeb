6c4335c8bd5b921a6d8a882e8f11d41ea6900300
mbdavid2 <mbdavid297@gmail.com>
2019-05-16 10:47:39 +0200

    Update CalculatorController.java

--- src/main/java/web/CalculatorController.java
+++ src/main/java/web/CalculatorController.java
@@ -12,37 +12,9 @@ public class CalculatorController {
         return "Salu2";
     }
 
-//    @RequestMapping("/{left}/{op}/{right}")
-//    public String OperationRequest(@PathVariable("left") Integer left, @PathVariable("operation") String operation, @PathVariable("right") Integer right) {
-//       return CalculatorService.performMathOperation(left, operation, right);
-//    }
-
-    @RequestMapping("/add/{left}/{right}")
-    public String performAddOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
-    	Integer operationResult = CalculatorService.operationAdd(left, right);
-    	return Integer.toString(operationResult);
-    }
-
-    @RequestMapping("/sub/{left}/{right}")
-    public String performSubOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
-    	Integer operationResult = CalculatorService.operationSub(left, right);
-    	return Integer.toString(operationResult);
-    }
-
-    @RequestMapping("/mul/{left}/{right}")
-    public String performMulOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
-    	Integer operationResult = CalculatorService.operationMul(left, right);
-    	return Integer.toString(operationResult);
-    }
-
-    @RequestMapping("/div/{left}/{right}")
-    public String performDivOperation(@PathVariable("left") Integer left, @PathVariable("right") Integer right) {
-    	if (right != 0) {
-    		Integer operationResult = CalculatorService.operationDiv(left, right);
-    		return Integer.toString(operationResult);
-    	}
-    	else {
-            return "Error: division by 0";
-        }
+    @PostMapping("/{expression}")
+    public String performAddOperation(@PathVariable("expression") String expression) {
+    	String operationResult = CalculatorService.computeExpression(expression);
+    	return operationResult;
     }
 }

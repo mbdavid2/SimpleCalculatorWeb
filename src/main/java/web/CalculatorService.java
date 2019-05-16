8d1a1bbe03a10deed9fec4854cba5cb47a73cd88
mbdavid2 <mbdavid297@gmail.com>
2019-05-16 10:48:06 +0200

    Update CalculatorService.java

--- src/main/java/web/CalculatorService.java
+++ src/main/java/web/CalculatorService.java
@@ -1,20 +1,25 @@
 package web;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import parserAntlr.CalculatorVisitorImpl;
+import parserAntlr.CalculatorLexer;
+import parserAntlr.CalculatorParser;
+
 public class CalculatorService {
 
-    public static Integer operationAdd(Integer a, Integer b) {
-    	return a + b;
-    }
+    public static String computeExpression(String expression) {
+    	CharStream input = CharStreams.fromString(expression);
+        Lexer lexer = new CalculatorLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CalculatorParser parser = new CalculatorParser(tokens);
+        ParseTree tree = parser.mainExpr();
 
-    public static Integer operationSub(Integer a, Integer b) {
-    	return a - b;
-    }
-
-    public static Integer operationMul(Integer a, Integer b) {
-       	return a / b;
-    }
-
-    public static Integer operationDiv(Integer a, Integer b) {
-    	return a * b;
+        CalculatorVisitorImpl calculator = new CalculatorVisitorImpl();
+        System.out.println("Result: " + calculator.visit(tree));
     }
 }
